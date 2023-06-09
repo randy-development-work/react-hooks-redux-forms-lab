18ae226433e0e879915c95bbac2bab21290c9eb7
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-09 14:18:16 +0300

    Set up addition and display of band names. This is integrated with Redux.

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({ onBandSubmit }) {
+  const [name, setName] = useState("");
+
+  function handleInputChange(event) {
+    setName(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    onBandSubmit(name);
+  }
+
+  return (
+    <div>
+      <p>Band Input</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Band Name
+          <input
+            type="text"
+            name="band"
+            value={name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Add Band</button>
+      </form>
+      Band Name: {name}
+    </div>
+  );
 }
 
 export default BandInput;
--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { bandAdded } from "./bandsSlice";
+import BandInput from "./BandInput";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const dispatch = useDispatch();
+  const bands = useSelector((state) => state.bands.entities);
+
+  function handleSubmit(name) {
+    dispatch(bandAdded(name));
+  }
+
+  const bandList = bands.map((band, index) => {
+    return <li key={index}>{band}</li>;
+  });
+  return (
+    <div>
+      <BandInput onBandSubmit={handleSubmit} />
+      <ul>{bandList}</ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
